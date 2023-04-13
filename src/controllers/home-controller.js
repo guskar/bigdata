d71ee29c1a5cf886d989e9e686630c6dbcad9b7b
guskar <gk222iv@student.lnu.se>
2023-04-13 09:10:58 +0200

    added template for express

---
+++ src/controllers/home-controller.js
@@ -0,0 +1,22 @@
+/**
+ * Home controller.
+ *
+ * @author Gustav Karlberg
+ * @version 1.0.0
+ */
+
+/**
+ * Encapsulates a controller.
+ */
+export class HomeController {
+  /**
+   * Renders a view and sends the rendered HTML string as an HTTP response.
+   * index GET.
+   *
+   * @param {object} req - Express request object.
+   * @param {object} res - Express response object.
+   * @param {Function} next - Express next middleware function.
+   */
+  async index (req, res, next) {
+  }
+}
---
+++ src/server.js
@@ -0,0 +1,118 @@
+/**
+ * The starting point of the application.
+ *
+ * @author Gustav Karlberg
+ * @version 1.0.0
+ */
+
+import express from 'express'
+import logger from 'morgan'
+import { dirname, join } from 'path'
+import { fileURLToPath } from 'url'
+import { router } from './routes/router.js'
+
+import helmet from 'helmet'
+
+try {
+  // Creates an Express application.
+  const app = express()
+
+  // Get the directory name of this module's path.
+  const directoryFullName = dirname(fileURLToPath(import.meta.url))
+  // Set the base URL to use for all relative URLs in a document.
+  // const baseURL = process.env.BASE_URL || '/'
+
+  // Set up a morgan logger using the dev format for log entries.
+  app.use(logger('dev'))
+
+  // use helmet for security.
+  app.use(helmet())
+  app.use(helmet.contentSecurityPolicy({ useDefaults: true }))
+
+  // View engine setup.
+  app.set('view engine', 'ejs')
+  app.set('views', join(directoryFullName, 'views'))
+
+  // Parse requests of the content type application/x-www-form-urlencoded.
+  // Populates the request object with a body object (req.body).
+  app.use(express.urlencoded({ extended: false }))
+
+  // Serve static files.
+  app.use(express.static(join(directoryFullName, '..', 'public')))
+
+  // Setup and use session middleware (https://github.com/expressjs/session)
+  // const sessionOptions = {
+  //   name: process.env.SESSION_NAME, // Don't use default session cookie name.
+  //   secret: process.env.SESSION_SECRET, // Change it!!! The secret is used to hash the session with HMAC.
+  //   resave: false, // Resave even if a request is not changing the session.
+  //   saveUninitialized: false, // Don't save a created but not modified session.
+  //   cookie: {
+  //     maxAge: 1000 * 60 * 60 * 24, // 1 day
+  //     sameSite: 'strict'
+  //   }
+  // }
+
+  if (app.get('env') === 'production') {
+    app.set('trust proxy', 1) // trust first proxy
+    // sessionOptions.cookie.secure = true // serve secure cookies
+  }
+
+  // app.use(session(sessionOptions))
+
+  // Middleware to be executed before the routes.
+  // app.use((req, res, next) => {
+  //   // Flash messages - survives only a round trip.
+  //   if (req.session.flash) {
+  //     res.locals.flash = req.session.flash
+  //     delete req.session.flash
+  //   }
+
+  //   // Pass the base URL to the views.
+  //   res.locals.baseURL = baseURL
+
+  //   next()
+  // })
+
+  // Register routes.
+  app.use('/', router)
+
+  // Error handler.
+  app.use(function (err, req, res, next) {
+    // 404 Not Found.
+    if (err.status === 404) {
+      return res
+        .status(404)
+        .sendFile(join(directoryFullName, 'views', 'errors', '404.html'))
+    }
+    // 403 Forbidden.
+    if (err.status === 403) {
+      return res
+        .status(403)
+        .sendFile(join(directoryFullName, 'views', 'errors', '403.html'))
+    }
+
+    // 500 Internal Server Error (in production, all other errors send this response).
+    if (req.app.get('env') !== 'development') {
+      return res
+        .status(500)
+        .sendFile(join(directoryFullName, 'views', 'errors', '500.html'))
+    }
+
+    // Development only!
+    // Only providing detailed error in development.
+
+    // Render the error page.
+    res
+      .status(err.status || 500)
+      .render('errors/error', { error: err })
+  })
+
+  // Starts the HTTP server listening for connections.
+  app.listen(process.env.PORT, () => {
+    console.log(`Server running at http://localhost:${process.env.PORT}`)
+    console.log('Press Ctrl-C to terminate...')
+  })
+} catch (err) {
+  console.error(err)
+  process.exitCode = 1
+}
