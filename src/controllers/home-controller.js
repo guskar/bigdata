12fd4241bc89bb95bf1570880b4d8c7882393b4b
guskar <gk222iv@student.lnu.se>
2023-04-14 09:35:39 +0200

    add homeservice

--- src/controllers/home-controller.js
+++ src/controllers/home-controller.js
@@ -1,67 +1,42 @@
 /**
- * Home controller.
+ * Module for the HomeController.
  *
  * @author Gustav Karlberg
  * @version 1.0.0
  */
-import { Client } from '@elastic/elasticsearch'
-import fs from 'fs'
+
+// import createError from 'http-errors'
+import { HomeService } from '../services/homeService.js'
 
 /**
  * Encapsulates a controller.
  */
 export class HomeController {
   /**
-   * Renders a view and sends the rendered HTML string as an HTTP response.
-   * index GET.
+   * The service.
+   *
+   * @type {HomeService}
+   */
+  #service
+
+  /**
+   * Initializes a new instance.
+   *
+   * @param {HomeService} service - A service instantiated from a class with the same capabilities as UserService.
+   */
+  constructor (service = new HomeService()) {
+    this.#service = service
+  }
+
+  /**
+   * Authenticates a user.
    *
    * @param {object} req - Express request object.
    * @param {object} res - Express response object.
    * @param {Function} next - Express next middleware function.
    */
   async index (req, res, next) {
-    const client = new Client({
-      node: 'https://localhost:9200',
-      auth: {
-        username: process.env.USERNAME,
-        password: process.env.PASSWORD
-      },
-      tls: {
-        ca: fs.readFileSync('/Users/gustavkarlberg/elasticsearch-8.7.0/config/certs/http_ca.crt'),
-        rejectUnauthorized: false
-      }
-    })
-
-    const response = await client.search({
-      index: 'gamesindex',
-      body: {
-        query: {
-          bool: {
-            must: [
-              {
-                match: {
-                  title: 'Final Fantasy VI'
-                }
-              },
-              {
-                range: {
-                  rating: {
-                    gte: 3.8
-                  }
-                }
-              }
-            ]
-          }
-        }
-      }
-    })
-
-    const hits = response.hits.hits
-
-    const data = hits.map(hit => ({
-      name: hit._source.title,
-      y: hit._source.rating
-    }))
+    const data = await this.#service.getData()
 
     res.render('home/index', { data })
   }
---
+++ src/services/homeService.js
@@ -0,0 +1,73 @@
+/**
+ * Module for the UsersService.
+ *
+ * @author Gustav Karlberg
+ * @version 1.0.0
+ */
+
+import { Client } from '@elastic/elasticsearch'
+import fs from 'fs'
+/**
+ * Encapsulates a home service.
+ */
+export class HomeService {
+  /**
+   * Initializes a new instance.
+   *
+   */
+  // constructor (repository = new UsersRepository()) {
+  //   super(repository)
+  // }
+
+  /**
+   * Gets elastic data.
+   *
+   * @returns {object} - boject representing elastic data.
+   */
+  async getData () {
+    const client = new Client({
+      node: 'https://localhost:9200',
+      auth: {
+        username: process.env.USERNAME,
+        password: process.env.PASSWORD
+      },
+      tls: {
+        ca: fs.readFileSync('/Users/gustavkarlberg/elasticsearch-8.7.0/config/certs/http_ca.crt'),
+        rejectUnauthorized: false
+      }
+    })
+
+    const response = await client.search({
+      index: 'gamesindex',
+      body: {
+        query: {
+          bool: {
+            must: [
+              {
+                match: {
+                  title: 'Final Fantasy VI'
+                }
+              },
+              {
+                range: {
+                  rating: {
+                    gte: 3.8
+                  }
+                }
+              }
+            ]
+          }
+        }
+      }
+    })
+
+    const hits = response.hits.hits
+
+    const data = hits.map(hit => ({
+      name: hit._source.title,
+      y: hit._source.rating
+    }))
+
+    return data
+  }
+}
